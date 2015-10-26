6d1e8774845ddb019cd695b2891923ec07256a8b
Alex Altieri & Clayton Easterbrooks <student@epicodus.com>
2015-10-26 11:15:31 -0700

    Allow user to add questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,24 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
 
   end
   def new
+    @question = Question.new
+  end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      flash[:notice] = "What are you doing"
+      render :create
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:option_one, :option_two, :image_one, :image_two)
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "250x200>", thumb: "100x100>" }, default_url: "/images/:style/tiger.jpg"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_presence_of :option_one, :option_two
+
+  has_attached_file :image_one, styles: { medium: "250x200>", thumb: "100x100>" }, default_url: "/images/:style/tiger.jpg"
+  validates_attachment_content_type :image_one, content_type: /\Aimage\/.*\Z/
+
+  has_attached_file :image_two, styles: { medium: "250x200>", thumb: "100x100>" }, default_url: "/images/:style/tiger.jpg"
+  validates_attachment_content_type :image_two, content_type: /\Aimage\/.*\Z/
 end
